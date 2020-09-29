bf39562f061f0f2b259568df2434336aa4f2dc49
arlex123 <arlexdavidsaavedra@hotmail.com>
2020-09-28 22:13:34 -0500

    firts commit

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { EventServiceService } from '../providers/event-service.service';
 
+import { Agenda } from '../models/estudiantesModel.model'
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {
+export class Tab2Page implements OnInit{
 
-  constructor() {}
+  public data: Array<Agenda>; 
 
+  constructor(private event: EventServiceService) { 
+    this.getData();
+  }
+
+  ngOnInit() {
+
+  }
+
+  public getData(){
+    this.event.getMock().
+    subscribe(resp => {
+      this.data = resp.agenda;
+      //console.log(resp);
+    }, error => {
+      console.error(error);
+    });
+  }
 }
--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { EventServiceService } from '../providers/event-service.service';
+
+import { Agenda } from '../models/estudiantesModel.model'
 
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss']
 })
-export class Tab3Page {
+export class Tab3Page implements OnInit{
 
-  constructor() {}
+  public data: Array<Agenda>; 
+
+  constructor(private event: EventServiceService) { 
+    this.getData();
+  }
+
+  ngOnInit() {
+
+  }
+
+  public getData(){
+    this.event.getMock().
+    subscribe(resp => {
+      this.data = resp.agenda;
+      //console.log(resp);
+    }, error => {
+      console.error(error);
+    });
+  }
 
 }
