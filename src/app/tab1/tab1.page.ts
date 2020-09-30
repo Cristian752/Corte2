be217057aa026ce28fd72e3c4be83bab1577b3e1
arlex123 <arlexdavidsaavedra@hotmail.com>
2020-09-29 21:19:27 -0500

    Segunda actualizacion

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,77 @@
-import { Component } from '@angular/core';
+import { NgForOf } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit{
+
+  data = [
+    {
+      value: 'Fiebre de 38°C o más',
+      selected: false
+    },
+    {
+      value: 'Dificultad para respirar',
+      selected: false
+    },
+    {
+      value: 'Dolor de garganta',
+      selected: false
+    },
+    {
+      value: 'Perdida del olfato',
+      selected: false
+    },
+    {
+      value: 'Tos',
+      selected: false
+    },
+    {
+      value: 'Fatiga o Decaimiento',
+      selected: false
+    },
+    {
+      value: 'Perdida del gusto',
+      selected: false
+    }
+  ];
+
+  dataYN = [
+    {
+      value: 'Si',
+      selected: true
+    },
+    {
+      value: 'No',
+      selected: false
+    }
+  ];
 
   constructor() {}
 
+  public datosSelect: Array<any>; 
+  public datosRadio: Array<any>; 
+
+  ngOnInit(){
+
+  }
+
+  onClick(check){
+    //console.log(check);
+    
+    this.datosSelect =  check ;
+  }
+
+  onClick2(rad){
+    //console.log(rad);
+    this.datosRadio = rad;
+  }
+
+  datos(){
+    console.log(this.datosRadio);
+    console.log(this.datosSelect);
+  }
 }
